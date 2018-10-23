f3b45699afde66d2707181c9363558745ba1b09c
Michael Ferguson <meister@spacekace.com>
2018-10-23 12:49:46 -0700

    register_then

--- app/controllers/swell_id/concerns/application_controller_concern.rb
+++ app/controllers/swell_id/concerns/application_controller_concern.rb
@@ -31,6 +31,38 @@ module SwellId
 					request.headers['CF-IPCountry']
 				end
 
+				def register_then
+
+					if params[:register_then].present?
+
+						user_params = params.require(:register_then).permit( :email, :full_name, :username )
+
+						user = User.where( email: user_params[:email] ).first
+						user ||= User.new( user_params.merge(ip: request.ip) )
+
+						if user.encrypted_password.present?
+							# this email is already registered for this site
+							set_flash "#{user.email} is already registered. Try <a href='/forgot'>Forgot Password</a>.", :error
+							redirect_back( fallback_location: '/register' )
+							return false
+						end
+
+						user.password = params[:register_then].require(:password)
+
+						if user.save
+							sign_in( :user, user )
+						else
+							set_flash "Could not register user.", :error, user
+							redirect_back( fallback_location: '/register' )
+							return false
+						end
+
+						log_event( { name: 'register', content: 'registered.' } )
+
+					end
+
+				end
+
 		end
 	end
 end
